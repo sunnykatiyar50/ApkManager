3a4d679114ac2f1027682b2959e0c2a5d5479c87
sunny <sunnykatiyar50@gmail.com>
2019-07-24 15:06:03 +0530

    Help PAge Updated... ListItems Explanantion

--- app/src/main/java/com/sunnykatiyar/skmanager/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/sunnykatiyar/skmanager/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.sunnykatiyar.skmanager.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1,R.string.tab_text_2,R.string.tab_text_3};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
